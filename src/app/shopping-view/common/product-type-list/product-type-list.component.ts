71827ccf7274ab2b565ca5ed37425327ab332d15
Indika Gunawardana <indika.gunawardana@gmail.com>
2020-05-26 06:13:38 +0530

    Style changes

--- src/app/shopping-view/common/product-type-list/product-type-list.component.ts
+++ src/app/shopping-view/common/product-type-list/product-type-list.component.ts
@@ -3,21 +3,21 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { CategoryService } from 'src/app/ws/category.service';
 
 @Component({
-  selector: 'app-product-type-list',
-  templateUrl: './product-type-list.component.html',
-  styleUrls: ['./product-type-list.component.scss']
+	selector: 'app-product-type-list',
+	templateUrl: './product-type-list.component.html',
+	styleUrls: ['./product-type-list.component.scss']
 })
 export class ProductTypeListComponent implements OnInit {
 
-  public categoryList;
+	public categoryList;
 
-  constructor(public router: Router, private categoryService: CategoryService, private activeRoute: ActivatedRoute) { }
+	constructor(public router: Router, private categoryService: CategoryService, private activeRoute: ActivatedRoute) { }
 
-  ngOnInit(): void {
-    this.categoryService.getProductCategories().subscribe((data: Object[]) => {
-      this.categoryList = data;
-    });
-  }
+	ngOnInit(): void {
+		this.categoryService.getProductCategories().subscribe((data: Object[]) => {
+			this.categoryList = data;
+		});
+	}
 
 
 }
