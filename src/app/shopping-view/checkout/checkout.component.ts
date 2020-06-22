701a82750f5d7cb2b4beb4e713abf8dc58ce4111
Indika Gunawardana <indika.gunawardana@gmail.com>
2020-06-23 05:06:52 +0530

    Modified the checkout

--- src/app/shopping-view/checkout/checkout.component.ts
+++ src/app/shopping-view/checkout/checkout.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { CartDataService } from 'src/app/service/cart-data.service';
+import { CartItem } from 'src/app/model/cart-item';
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+	selector: 'app-checkout',
+	templateUrl: './checkout.component.html',
+	styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+	cartItems: CartItem[];
+	total: number = 0.00;
 
-  constructor() { }
+	constructor(private cart: CartDataService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.cartItems = this.cart.cart.items;
+		this.calculateTotal(this.cartItems);
+	}
+
+	calculateTotal(items: CartItem[]) {
+		this.total = 0;
+		items.forEach(item => {
+			this.total += item.product.unit_price_lkr.valueOf() * item.quantity.valueOf();
+		});
+	}
 
 }
