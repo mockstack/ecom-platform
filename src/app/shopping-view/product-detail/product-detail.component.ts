e9601e7c7e6b9b3f81e7f0a365c27382f525f2ff
Indika Gunawardana <indika.gunawardana@gmail.com>
2020-09-01 21:20:48 +0530

    Change add to cart button behaviours

--- src/app/shopping-view/product-detail/product-detail.component.ts
+++ src/app/shopping-view/product-detail/product-detail.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Route } from '@angular/compiler/src/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/model/product';
 import { ProductService } from 'src/app/ws/product.service';
 import { environment } from 'src/environments/environment';
 import { CartDataService } from 'src/app/service/cart-data.service';
 import { CartItem } from 'src/app/model/cart-item';
+import { IBuyItem } from 'src/app/model/ibuy-item';
 
 @Component({
 	selector: 'app-product-detail',
@@ -14,10 +14,11 @@ import { CartItem } from 'src/app/model/cart-item';
 })
 export class ProductDetailComponent implements OnInit {
 
-	productId: string;
-	product: Product;
-	quantity: number = 1;
-	serverUrl = environment.apiUrl;
+	private productId: string;
+	public product: Product;
+	public quantity: number = 1;
+	public serverUrl = environment.apiUrl;
+	public isAlreadyAdded: boolean;
 
 	constructor(private router: Router, private activatedRoute: ActivatedRoute,
 		private productService: ProductService, private cartDataService: CartDataService) { }
@@ -26,17 +27,29 @@ export class ProductDetailComponent implements OnInit {
 		this.activatedRoute.params.subscribe(params => {
 			this.productId = params['id'];
 
-			this.productService.getProduct(this.productId).subscribe((product: Product) => {
-				this.quantity = 1;
-				this.product = product;
-			}, error => {
-				this.router.navigateByUrl('404');
-			})
+			let cartItems = this.cartDataService.cart.items.filter((item: IBuyItem) => {
+				return item.buyItem.product._id === this.productId;
+			});
+
+			if (cartItems.length === 1) {
+				this.product = cartItems[0].buyItem.product;
+				this.quantity = cartItems[0].buyItem.quantity;
+				this.isAlreadyAdded = true;
+			} else {
+				this.productService.getProduct(this.productId).subscribe((product: Product) => {
+					this.quantity = 1;
+					this.product = product;
+					this.isAlreadyAdded = false;
+				}, error => {
+					this.router.navigateByUrl('404');
+				});
+			}
 		});
 	}
 
 	increaseQuantity() {
 		this.quantity++;
+		this.cartDataService.updateQuantiryByProductId(this.product._id.valueOf(), this.quantity);
 	}
 
 	decreaseQuantity() {
@@ -44,9 +57,17 @@ export class ProductDetailComponent implements OnInit {
 		if (this.quantity <= 1) {
 			this.quantity = 1;
 		}
+		this.cartDataService.updateQuantiryByProductId(this.product._id.valueOf(), this.quantity);
 	}
 
 	addItemToCart(product: Product) {
 		this.cartDataService.addItem(new CartItem(product, this.quantity));
+		this.isAlreadyAdded = true;
+	}
+
+	removeItemFromCart() {
+		this.cartDataService.removeItem(this.productId);
+		this.isAlreadyAdded = false;
+		this.quantity = 1;
 	}
 }
